e0f2e1473d04b627a4fe86d535754fca8e71edd7
gearbird <52440839+gearbird@users.noreply.github.com>
2020-09-10 02:57:41 -0700

    Clean main file

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -5,10 +5,3 @@ using namespace leetcode;
 int main() {
     test_reorderLogFiles();
 }
-
-void archived_test() {
-    // list of tests history
-    // test_twoSum();
-    // test_addTwoNumbers();
-    // test_lengthOfLongestSubstring();
-}
