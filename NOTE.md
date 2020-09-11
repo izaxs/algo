80ae0b08e138ffff598d91a31711a22329e357f4
gearbird <52440839+gearbird@users.noreply.github.com>
2020-09-10 19:28:26 -0700

    Add LC0005, add LeetCode programming error note

---
+++ NOTE.md
@@ -0,0 +1,24 @@
+# Leetcode Programming Mistake Statistics
+
+<table>
+    <tr>
+        <td>Error Type</td>
+        <td>Frequency</td>
+    </tr>
+    <tr>
+        <td>Index & offset target wrong</td>
+        <td>2</td>
+    </tr>
+    <tr>
+        <td>Index out of range</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td>Forget to add item to container in loop</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td>Local variable not initialized</td>
+        <td>1</td>
+    </tr>
+</table>
--- leetcode/solution/0005.cpp
+++ leetcode/solution/0005.cpp
@@ -1,13 +1,56 @@
 #include "../include/common.hpp"
 
 namespace leetcode {
+    using std::string;
+
+    void expand(string &s, int lo, int hi, int &maxLo, int &maxLen) {
+        while (lo >= 0 && hi < s.size()) {
+            if (s[lo] != s[hi]) return;
+            int curLen = hi - lo + 1;
+            if (curLen > maxLen) {
+                maxLo = lo;
+                maxLen = curLen;
+            }
+            lo--, hi++;
+        }
+    }
+
     string longestPalindrome(string s) {
-        return s;
+        int maxLo = 0, maxLen = 0, size = s.size();
+        for (int i = 0; i < size; i++) {
+            if (i > size / 2 && std::min(i + 1, size - i) * 2 <= maxLen) break;
+            expand(s, i, i, maxLo, maxLen);
+            expand(s, i, i + 1, maxLo, maxLen);
+        }
+        return s.substr(maxLo, maxLen);
     }
 
     void test_longestPalindrome() {
-        string input = "aabcbefebcde";
+        string input = "babad";
         string output = longestPalindrome(input);
         print_inputoutput(input, output);
+
+        // string input = "aabcbefebcde";
+        // string output = longestPalindrome(input);
+        // print_inputoutput(input, output);
     }
+
+    // Bad solution, time exceeded
+    // bool isPalindrome(string s, int lo, int hi) {
+    //     while (lo < hi) {
+    //         if (s[lo++] != s[hi--]) return false;
+    //     }
+    //     return true;
+    // }
+
+    // string longestPalindrome(string s) {
+    //     int len = s.size();
+    //     while (len > 0) {
+    //         for (int i = 0; i + len <= s.size(); i++) {
+    //             if (isPalindrome(s, i, i + len - 1)) return s.substr(i, len);
+    //         }
+    //         len--;
+    //     }
+    //     return "";
+    // }
 }
