a8ceb1631e28b52f43ff80f3a23b7091b6766303
gearbird <52440839+gearbird@users.noreply.github.com>
2020-09-12 15:34:47 -0700

    Add LC0006

--- leetcode/list.hpp
+++ leetcode/list.hpp
@@ -5,5 +5,6 @@ namespace leetcode {
     extern void test_addTwoNumbers();                           // 0002
     extern void test_lengthOfLongestSubstring();                // 0003
     extern void test_longestPalindrome();                       // 0005
+    extern void test_convertZigZag();                           // 0006
     extern void test_reorderLogFiles();                         // 0937
 }
---
+++ leetcode/solution/0006.cpp
@@ -0,0 +1,27 @@
+#include "../include/common.hpp"
+
+namespace leetcode {
+    using std::string;
+    using std::vector;
+
+    string convertZigZag(string s, int numRows) {
+        if (numRows <= 1) return s;
+        vector<string> zigzag(numRows, "");
+        for (int i = 0, r = 0, nextR = 0; i < s.size(); i++) {
+            zigzag[r] += s[i];
+            if (r == numRows - 1) nextR = -1;
+            if (r == 0) nextR = 1;
+            r += nextR;
+        }
+        string output;
+        for (auto s: zigzag) output += s;
+        return output;
+    }
+
+    void test_convertZigZag() {
+        string input = "PAYPALISHIRING";
+        auto output = convertZigZag(input, 3);
+        print_inputoutput(input, output);
+    }
+
+}
