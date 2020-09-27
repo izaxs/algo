5118f27ccce7490a51fa55ae75c2313b60d83c3a
gearbird <52440839+gearbird@users.noreply.github.com>
2020-09-26 17:46:53 -0700

    Update LC0125

--- leetcode/0125.cpp
+++ leetcode/0125.cpp
@@ -16,11 +16,21 @@ namespace leetcode {
     bool isPalindrome(string s) {
         return isPalindromeHelper(s, 0, s.size() - 1);
     }
+
+    bool isPalindrome2(string s) {
+        int lo = 0, hi = s.size() - 1;
+        while (lo < hi) {
+            if (!std::isalnum(s[lo])) lo++;
+            else if (!std::isalnum(s[hi])) hi--;
+            else if (tolower(s[lo++]) != tolower(s[hi--])) return false;
+        }
+        return true;
+    }
 }
 
 int main() {
     using namespace leetcode;
     string input = "A man, a plan, a canal: Panama";
-    bool output = isPalindrome(input);
+    bool output = isPalindrome2(input);
     print_inputoutput(input, to_string(output));
 }
