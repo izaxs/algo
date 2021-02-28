11773bd900beb24aec1c4986186724d11945533b
gearbird <52440839+gearbird@users.noreply.github.com>
2021-02-28 00:01:34 -0600

    Add Go implementation for IntHeap

---
+++ gocode/lib/Equality.go
@@ -0,0 +1,16 @@
+package lib
+
+// IsSameIntSlice checks if slices are equal
+func IsSameIntSlice(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
