03c3213de53165213c65fab8e08a5b5f02272a6a
gearbird <52440839+gearbird@users.noreply.github.com>
2021-03-13 17:57:32 -0600

    Add LC0014

---
+++ snakecode/0014.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+
+    pad = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        if not digits:
+            return res
+        res.append('')
+        for d in digits:
+            newRes = []
+            for comb in res:
+                for alpha in self.pad[int(d)]:
+                    newRes.append(comb + alpha)
+            res = newRes
+        return res
