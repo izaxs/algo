d230912e22e849098e9d6190fc0c5219dd8d2dc7
gearbird <52440839+gearbird@users.noreply.github.com>
2021-03-16 22:47:14 -0500

    Add LC0014

---
+++ snakecode/0014.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+        base = min(strs, key=len)
+        for i in range(len(base)):
+            for s in strs:
+                if base[i] != s[i]:
+                    return base[:i]
+        return base
+        
