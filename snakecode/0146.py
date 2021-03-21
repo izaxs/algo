75679f224cd1c7c565f87fb885f44f80f74ad523
gearbird <52440839+gearbird@users.noreply.github.com>
2021-03-21 00:46:00 -0500

    Add LC0146

---
+++ snakecode/0146.py
@@ -0,0 +1,16 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        pass
+
+    def get(self, key: int) -> int:
+        pass
+
+    def put(self, key: int, value: int) -> None:
+        pass
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
