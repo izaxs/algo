26a54cfd7fee5c12697ed01056d6c50e400305ff
gearbird <52440839+gearbird@users.noreply.github.com>
2021-04-06 01:56:07 -0500

    Add LC1041

---
+++ snakecode/1041.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        directions = [complex(0, 1), complex(-1, 0), complex(0, -1), complex(1, 0)]
+        dirIndex, coordinate = 0, complex(0, 0)
+        for c in instructions:
+            if c == 'R':
+                dirIndex += 1
+            elif c == 'L':
+                dirIndex += 3
+            else:
+                coordinate += directions[dirIndex]
+                continue
+            dirIndex %= 4
+        return coordinate == complex(0, 0) or dirIndex != 0
+
+s = Solution()     
+dirs = "GGLLGG"
+print(s.isRobotBounded(dirs))
