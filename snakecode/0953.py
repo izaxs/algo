255e1c5495020a781dd7fa85a00a03b57d2f212d
gearbird <52440839+gearbird@users.noreply.github.com>
2021-05-17 01:36:04 -0500

    Add LC0953

---
+++ snakecode/0953.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        orderMap = {c: i for i, c in enumerate(order)}
+        for w1, w2 in zip(words, words[1:]):
+            i, minLen, isSmaller = 0, min(len(w1), len(w2)), False
+            while i < minLen:
+                cmp = orderMap[w2[i]] - orderMap[w1[i]]
+                if cmp == 0:
+                    i += 1
+                    continue
+                if cmp < 0:
+                    isSmaller = True
+                    break
+                if cmp > 0:
+                    return False
+            if not isSmaller and len(w1) > len(w2):
+                return False
+        return True
+
+s = Solution()
+s.isAlienSorted(["hello","leetcode"], "hlabcdefgijkmnopqrstuvwxyz")
