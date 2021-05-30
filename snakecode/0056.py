e1140dc8ba7e962d318cb98fdf1e8d550cabbb25
gearbird <52440839+gearbird@users.noreply.github.com>
2021-05-30 18:49:55 -0500

    Update LC0056

--- snakecode/0056.py
+++ snakecode/0056.py
@@ -3,9 +3,9 @@ from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x: (x[0], x[1]))
-        merged = [intervals[0]]
+        merged = []
         for i in intervals:
-            if merged[-1][1] < i[0]:
+            if not merged or merged[-1][1] < i[0]:
                 merged.append(i)
             else:
                 merged[-1][1] = max(merged[-1][1], i[1])
