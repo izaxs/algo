8a2d1bf53cb5106f81f1467986c7b503b8b78673
gearbird <52440839+gearbird@users.noreply.github.com>
2021-05-30 20:55:39 -0500

    Add LC0238

--- snakecode/0238.py
+++ snakecode/0238.py
@@ -1,3 +1,11 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        pass
+        products, it = [1] * len(nums), 1
+        for i in range(len(nums) - 1, -1, -1):
+            products[i] = it
+            it *= nums[i]
+        it = 1
+        for i in range(len(products)):
+            products[i] *= it
+            it *= nums[i]
+        return products;
