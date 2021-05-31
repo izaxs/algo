bfe38c83c1b6c745ed6b95390c4e30554ed9401f
gearbird <52440839+gearbird@users.noreply.github.com>
2021-05-30 22:04:53 -0500

    Add LC0252

---
+++ snakecode/0252.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canAttendMeetings(self, intervals: list[list[int]]) -> bool:
+        intervals.sort(key=lambda x: x[0])
+        last = [-1, -1]
+        for i in intervals:
+            if last[1] <= i[0]:
+                last = i
+            else:
+                return False
+        return True
