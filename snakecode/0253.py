6134e0debedc26f44d66c41c3f1fda3cdc5b1136
gearbird <52440839+gearbird@users.noreply.github.com>
2021-05-30 22:48:38 -0500

    Update LC0253

--- snakecode/0253.py
+++ snakecode/0253.py
@@ -11,6 +11,27 @@ class Solution:
             maxRooms = max(maxRooms, len(using))
         return maxRooms
 
+class Solution2:
+    def minMeetingRooms(self, intervals: list[list[int]]) -> int:
+        starts: list[int] = [0] * len(intervals)
+        ends: list[int] = [0] * len(intervals)
+        for i, v in enumerate(intervals):
+            starts[i], ends[i] = v[0], v[1]
+        starts.sort()
+        ends.sort()
+        s, e = 0, 0
+        maxRooms, using = 0, 0
+        while s < len(starts):
+            if starts[s] < ends[e]:
+                using += 1
+                s += 1
+            else:
+                using -= 1
+                e += 1
+            maxRooms = max(maxRooms, using)
+        return maxRooms
+
+
 s = Solution()
 intervals = [[0,30],[5,10],[15,20]]
 r = s.minMeetingRooms(intervals)
