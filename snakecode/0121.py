6ba384780668ad135bc495acd713e2f7eea898d3
gearbird <52440839+gearbird@users.noreply.github.com>
2021-05-30 19:00:06 -0500

    Add LC0121

--- snakecode/0121.py
+++ snakecode/0121.py
@@ -2,4 +2,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        res, curMin = 0, prices[0]
+        for p in prices:
+            if p >= curMin:
+                res = max(res, p - curMin)
+            else:
+                curMin = p
+        return res
