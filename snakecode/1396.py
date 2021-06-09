46c2a19e7a3cfacf1087967a14be38efcede668e
gearbird <52440839+gearbird@users.noreply.github.com>
2021-06-09 04:22:45 -0500

    Update LC1396

--- snakecode/1396.py
+++ snakecode/1396.py
@@ -1,13 +1,19 @@
 class UndergroundSystem:
 
     def __init__(self):
-        pass
+        self.time = {} # {(startStation, endStation) : (averageTime, count)}
+        self.trip = {} # {id : (startStation, startTime)}
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        pass
+        self.trip[id] = (stationName, t)
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        pass
+        startStation, checkInTime = self.trip[id]
+        timeUsed = t - checkInTime
+        averageTime, count = self.time.get((startStation, stationName), (0, 0))
+        averageTime = (averageTime * count + timeUsed) / (count + 1)
+        self.time[(startStation, stationName)] = (averageTime, count + 1)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        pass
+        averageTime, _ = self.time.get((startStation, endStation), (0, 0))
+        return averageTime
