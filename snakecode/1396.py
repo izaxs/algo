d68a2982393f8a0da4638e4c83f2d45eab9de594
gearbird <52440839+gearbird@users.noreply.github.com>
2021-06-08 23:14:44 -0500

    Add LC1396

---
+++ snakecode/1396.py
@@ -0,0 +1,13 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        pass
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        pass
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        pass
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        pass
