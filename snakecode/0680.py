15a0304826b959eab239bc2fedba6122b3086dd9
gearbird <52440839+gearbird@users.noreply.github.com>
2021-06-15 22:52:58 -0500

    Add LC0680

---
+++ snakecode/0680.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        # return self.helper(s, 0, len(s)-1, 1)
+        return self.helper2(s, 0, len(s)-1, 1)
+
+    def helper(self, s: str, lo: int, hi: int, k: int):
+        if lo >= hi:
+            return True
+        if s[lo] != s[hi]:
+            if k > 0:
+                return self.helper(s, lo+1, hi, k-1) or self.helper(s, lo, hi-1, k-1)
+            else:
+                return False
+        return self.helper(s, lo+1, hi-1, k)
+
+    def helper2(self, s: str, lo: int, hi: int, k: int):
+        while lo < hi:
+            if s[lo] == s[hi]:
+                lo += 1
+                hi -= 1
+            elif k > 0:
+                return self.helper2(s, lo+1, hi, k-1) or self.helper2(s, lo, hi-1, k-1)
+            else:
+                return False
+        return True
