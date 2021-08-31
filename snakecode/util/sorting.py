dc657c7682a3b1c1c40ad0efb2f96f117f7531ea
gearbird <52440839+gearbird@users.noreply.github.com>
2021-08-31 05:03:25 -0500

    Add quick sort lib

--- snakecode/util/sorting.py
+++ snakecode/util/sorting.py
@@ -22,20 +22,39 @@ def insertionSort(nums: list[int]):
                 break
             nums[j-1], nums[j] = nums[j], nums[j-1]
 
-def quickSort(nums: list[int]):
-    pass
+def quickSort(nums: list[int], lo: int = 0, hi: int = -1):
+    def partition(nums: list[int], lo: int, hi: int) -> int:
+        # return pivot index, left[] < pivot <= right[]
+        lower = lo
+        for i in range(lo, hi):
+            if nums[i] < nums[hi]:
+                nums[lower], nums[i] = nums[i], nums[lower]
+                lower += 1
+        nums[lower], nums[hi] = nums[hi], nums[lower]
+        return lower
+    if hi == -1: hi = len(nums)-1
+    if lo >= hi: return
+    pivot = partition(nums, lo, hi)
+    quickSort(nums, lo, pivot-1)
+    quickSort(nums, pivot+1, hi)
 
 def mergeSort(nums: list[int]):
-    pass
+    
 
 if __name__ == '__main__':
     unsorted = [4, 13, 8, 10, 6, 0, 7, 5, 9, 4, 1, 14, 8, 2]
     # bubbleSortCase = unsorted[:]
     # bubbleSort(bubbleSortCase)
     # print(bubbleSortCase)
+
     # selectionSortCase = unsorted[:]
     # selectionSort(selectionSortCase)
     # print(selectionSortCase)
-    insertionSortCase = unsorted[:]
-    insertionSort(insertionSortCase)
-    print(insertionSortCase)
+
+    # insertionSortCase = unsorted[:]
+    # insertionSort(insertionSortCase)
+    # print(insertionSortCase)
+
+    quickSortCase = unsorted[:]
+    quickSort(quickSortCase)
+    print(quickSortCase)
