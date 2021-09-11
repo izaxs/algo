be20263b5f4a1026f9b9cd6b5791487d72d9bf8e
gearbird <52440839+gearbird@users.noreply.github.com>
2021-09-11 17:37:24 -0500

    Add LC0445

---
+++ snakecode/0445.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val: int = 0, next: Optional[ListNode] = None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        s1: list[int] = []
+        s2: list[int] = []
+        while l1 is not None:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2 is not None:
+            s2.append(l2.val)
+            l2 = l2.next
+        curNode: Optional[ListNode] = None
+        carry = 0
+        while s1 or s2 or carry:
+            if s1: carry += s1.pop()
+            if s2: carry += s2.pop()
+            curNode = ListNode(carry % 10, curNode)
+            carry //= 10
+        return curNode
