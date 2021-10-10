dcdea7ab6f5d71eff7b56aab6b0edfb98d5f159d
gearbird <52440839+gearbird@users.noreply.github.com>
2021-10-09 23:34:19 -0500

    Add Golang string_play

---
+++ .archived/gocode/other/string_play.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(string("Hello"[1]))              // ASCII only
+    fmt.Println(string([]rune("Hello, 世界")[1])) // UTF-8
+    fmt.Println(string([]rune("Hello, 世界")[8])) // UTF-8
+}
