421fa98327d54c57592d4b494f27fb7becc83277
gearbird <52440839+gearbird@users.noreply.github.com>
2021-10-15 03:35:03 -0500

    Add log decorator

---
+++ snakecode/lang/log_decorator.py
@@ -0,0 +1,19 @@
+# type: ignore
+
+def log_it(prefix: str, suffix: str):
+    def deco(func):
+        def updated_func(*args, **kargs):
+            print(prefix)
+            res = func(*args, **kargs)
+            print(suffix)
+            return res
+        return updated_func
+    return deco
+
+@log_it(prefix='pre', suffix='after')
+def prog(a: int, b: int) -> int:
+    c = a + b
+    print(f'a + b = {c}')
+    return c
+
+prog(1, 2)
