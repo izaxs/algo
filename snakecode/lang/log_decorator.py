a9c33bede510222a19aae62e5db826c783393f43
gearbird <52440839+gearbird@users.noreply.github.com>
2021-10-15 03:38:26 -0500

    Update log decorator

--- snakecode/lang/log_decorator.py
+++ snakecode/lang/log_decorator.py
@@ -1,7 +1,9 @@
 # type: ignore
+from functools import wraps
 
 def log_it(prefix: str, suffix: str):
     def deco(func):
+        @wraps(func)
         def updated_func(*args, **kargs):
             print(prefix)
             res = func(*args, **kargs)
