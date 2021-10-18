a42bd6097935857e4d3554f906c199173b1eab4b
gearbird <52440839+gearbird@users.noreply.github.com>
2021-10-17 22:30:37 -0500

    Update LC0560

--- snakecode/0560.py
+++ snakecode/0560.py
@@ -10,12 +10,21 @@ class Solution:
             expects[k+curSum] = count + 1
         return result
 
-    def subarraySum2(self, nums: list[int], k: int) -> int:
-        curSum, result = 0, 0
-        preSum: dict[int, int] = {0: 1}
+    # def subarraySum2(self, nums: list[int], k: int) -> int:
+    #     curSum, result = 0, 0
+    #     preSum: dict[int, int] = {0: 1}
+    #     for n in nums:
+    #         curSum += n
+    #         # preSum is used for later iterations, can not affect this round
+    #         preSum[curSum] = preSum.get(curSum, 0)+1
+    #         result += preSum.get(curSum-k, 0)
+    #     return result
+
+    def subarraySum3(self, nums: list[int], k: int) -> int:
+        preSums = {0: 1}
+        curSum, count = 0, 0
         for n in nums:
             curSum += n
-            # preSum is used for later iterations, can not affect this round
-            preSum[curSum] = preSum.get(curSum, 0)+1
-            result += preSum.get(curSum-k, 0)
-        return result
+            count += preSums.get(curSum-k, 0)
+            preSums[curSum] = preSums.get(curSum, 0)+1
+        return count
