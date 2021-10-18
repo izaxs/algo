562200231c07e35b87e925f109bea2c4f3a0c9aa
gearbird <52440839+gearbird@users.noreply.github.com>
2021-10-17 22:09:50 -0500

    Update LC0042

--- snakecode/0042.py
+++ snakecode/0042.py
@@ -13,5 +13,22 @@ class Solution:
             globalHigh = max(globalHigh, curMin)
         return trapped
 
+    def trap2(self, height: list[int]) -> int:
+        wall, bot = 0, 0
+        lo, hi = 0, len(height)-1
+        trapped = 0
+        while lo < hi:
+            hLo, hHi = height[lo], height[hi]
+            if hLo < hHi:
+                bot = hLo
+                wall = max(wall, hLo)
+                lo += 1
+            else:
+                bot = hHi
+                wall = max(wall, hHi)
+                hi -= 1
+            trapped += max(0, wall-bot)
+        return trapped
+
 s = Solution()
 print(s.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
