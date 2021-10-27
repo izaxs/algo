9d7254ad136cb4563a2ed23aa0f51a055f1d6588
gearbird <52440839+gearbird@users.noreply.github.com>
2021-10-27 00:44:19 -0500

    Update LC0072

--- snakecode/0072.py
+++ snakecode/0072.py
@@ -1,3 +1,12 @@
+# Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+# You have the following three operations permitted on a word:
+
+#     Insert a character
+#     Delete a character
+#     Replace a character
+
+
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         if not word1 and not word2: return 0
