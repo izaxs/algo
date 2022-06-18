97c47b57fa1d5988ff5493b7058b3813e6c16fac
izaxs <52440839+izaxs@users.noreply.github.com>
2022-06-18 00:37:07 -0500

    Update LC0002

--- SharpCode/H00/S0002.cs
+++ SharpCode/H00/S0002.cs
@@ -19,6 +19,25 @@ public class S0002
         return head.next;
     }
 
+    public ListNode? AddTwoNumbersSmall(ListNode? l1, ListNode? l2)
+    {
+        var head = new ListNode();
+        var cur = head;
+        var carry = 0;
+        while (l1 is not null || l2 is not null || carry != 0)
+        {
+            carry += l1?.val ?? 0;
+            carry += l2?.val ?? 0;
+            cur.next = l1 ?? l2 ?? new ListNode();
+            cur.next.val = carry % 10;
+            carry /= 10;
+            l1 = l1?.next;
+            l2 = l2?.next;
+            cur = cur.next;
+        }
+        return head.next;
+    }
+
     public ListNode? AddTwoNumbersFast(ListNode? l1, ListNode? l2)
     {
         var head = new ListNode();
