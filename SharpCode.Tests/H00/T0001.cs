7399a66d9c003d53067275581a3360424ba13358
izaxs <52440839+izaxs@users.noreply.github.com>
2022-06-18 00:31:27 -0500

    Change C# format, add LC0002

--- SharpCode.Tests/H00/T0001.cs
+++ SharpCode.Tests/H00/T0001.cs
@@ -1,8 +1,10 @@
 namespace SharpCode.Tests.H00;
 
-public class T0001 {
+public class T0001
+{
     [Fact]
-    public void TestS0001() {
+    public void TestS0001()
+    {
         var sln = new S0001();
         var res = sln.TwoSum(new int[] { 2, 4, 1, 9, 7, 3 }, 8);
         Assert.Equal(new int[] { 2, 4 }, res);
--- SharpCode/H00/S0001.cs
+++ SharpCode/H00/S0001.cs
@@ -1,10 +1,14 @@
 ﻿namespace SharpCode;
 
-public class S0001 {
-    public int[] TwoSum(int[] nums, int target) {
+public class S0001
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
         var expect = new Dictionary<int, int>();
-        for (int i = 0; i < nums.Length; i++) {
-            if (expect.TryGetValue(nums[i], out int last)) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (expect.TryGetValue(nums[i], out int last))
+            {
                 return new int[2] { last, i };
             }
             expect[target - nums[i]] = i;
