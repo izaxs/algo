3948adcd08f1d9ace7d030beb4f7c99d370385e8
izaxs <52440839+izaxs@users.noreply.github.com>
2022-07-16 00:17:45 -0500

    Reorg C# tests

--- SharpCode.Test/T00.cs
+++ SharpCode.Test/T00.cs
@@ -1,7 +1,18 @@
-namespace SharpCode;
+namespace SharpCode.Test;
 
-public class T0009
+/*
+ * Tests for S0001 ~ S0099
+ */
+public class T00
 {
+    [Fact]
+    public void TestS0001()
+    {
+        var sln = new S0001();
+        var res = sln.TwoSum(new int[] { 2, 4, 1, 9, 7, 3 }, 8);
+        Assert.Equal(new int[] { 2, 4 }, res);
+    }
+
     [Fact]
     public void TestS0009()
     {
