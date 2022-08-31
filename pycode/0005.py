59bac33ecd96e4c684181b8467dc3af5e17b4bb3
izaxs <52440839+izaxs@users.noreply.github.com>
2022-08-30 19:42:45 -0700

    Update LC0005

--- pycode/0005.py
+++ pycode/0005.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) <= 1:
@@ -17,3 +20,25 @@ class Solution:
         while lo >= 0 and hi < len(s) and s[lo] == s[hi]:
             lo, hi = lo - 1, hi + 1
         return (lo + 1, hi - 1)  # return last valid indexes
+
+    def longestPalindrome2(self, s: str) -> str:
+        if len(s) <= 1: return s
+        self.resLo, self.resHi = 0, 0
+        for center in range(0, len(s)):
+            for offset in [0, 1]:
+                if self.shouldSkip(len(s), center, center+offset): continue
+                curLo, curHi = self.expand2(s, center, center+offset)
+                if curHi - curLo > self.resHi - self.resLo:
+                    self.resLo, self.resHi = curLo, curHi
+        return s[self.resLo:self.resHi+1]
+
+    def expand2(self, s: str, lo: int, hi: int) -> tuple[int, int]:
+        while lo >= 0 and hi < len(s) and s[lo] == s[hi]:
+            lo, hi = lo-1, hi+1
+        return (lo+1, hi-1)
+
+    def shouldSkip(self, sLen: int, lo: int, hi: int) -> bool:
+        maxHalf = min(lo, sLen-1-hi)
+        maxLen = hi-lo+maxHalf*2+1
+        return maxLen <= (self.resHi - self.resLo)
+    
