e28f6fae8849aa1ac4f2fd51b49277ce96f56d72
izaxs <52440839+izaxs@users.noreply.github.com>
2022-11-29 22:14:49 -0500

    Update LC0014

--- pycode/0014.py
+++ pycode/0014.py
@@ -8,3 +8,12 @@ class Solution:
                 if base[i] != s[i]:
                     return base[:i]
         return base
+
+    def longestCommonPrefix2(self, strs: list[str]) -> str:
+        if not str: return ''
+        effectiveBase = min(strs, key=len)
+        for i in range(len(effectiveBase)):
+            for s in strs:
+                if s[i] != effectiveBase[i]:
+                    return effectiveBase[:i]
+        return effectiveBase
