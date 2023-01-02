6aeb84958cabfa9b7b8583db35436b2f55b25792
izaxs <52440839+izaxs@users.noreply.github.com>
2023-01-02 13:02:57 -0800

    Update TypeScript code with strict type check

--- tscode/00/01.ts
+++ tscode/00/01.ts
@@ -2,7 +2,7 @@ function twoSum(nums: number[], target: number): number[] {
     let seen = new Map<number, number>();
     for (let [index, value] of nums.entries()) {
         if (seen.has(value)) {
-            return [seen.get(value), index];
+            return [seen.get(value)!, index];
         }
         seen.set(target-value, index);
     }
