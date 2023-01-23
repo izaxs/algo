d999040c1926b0aa0799584b22de36c3f2f33ad9
izaxs <52440839+izaxs@users.noreply.github.com>
2023-01-22 18:33:58 -0600

    Update LC 0142

--- pycode/01/0142.py
+++ pycode/01/0142.py
@@ -28,6 +28,31 @@ class Solution:
         while slow != fast:
             if not fast or not fast.next:
                 return None
+            assert slow
             slow = slow.next
             fast = fast.next.next
-        slow = 
+        slow = fake
+        while slow != fast:
+            assert slow and fast
+            slow = slow.next
+            fast = fast.next
+        return slow
+
+    def detectCycle3(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def tryMeet(head: Optional[ListNode]) -> Optional[ListNode]:
+            slow = fast = head
+            while fast and fast.next:
+                assert slow
+                slow = slow.next
+                fast = fast.next.next
+                if slow == fast:
+                    return fast
+            return None
+        
+        meet = tryMeet(head)
+        if not meet:
+            return None
+        while head != meet:
+            assert head and meet
+            head, meet = head.next, meet.next
+        return head
