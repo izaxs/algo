8ee1c31bf0199c50b60690798f2259a2b5da2891
izaxs <52440839+izaxs@users.noreply.github.com>
2023-01-23 18:20:49 -0800

    Update LC 0121

--- pycode/01/0121.py
+++ pycode/01/0121.py
@@ -1,3 +1,11 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+# Constraints:
+# 1 <= prices.length <= 105
+# 0 <= prices[i] <= 104
+
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         res, curMin = 0, prices[0]
@@ -7,3 +15,11 @@ class Solution:
             else:
                 curMin = p
         return res
+
+    def maxProfit2(self, prices: list[int]) -> int:
+        minPrice = prices[0]
+        maxProfit = 0
+        for p in prices:
+            minPrice = min(minPrice, p)
+            maxProfit = max(maxProfit, p-minPrice)
+        return maxProfit
