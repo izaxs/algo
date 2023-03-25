8288cdd43e91093ae5832669ef1295c132b179b2
izaxs <52440839+izaxs@users.noreply.github.com>
2023-03-25 00:47:49 -0500

    Add LC0855

---
+++ pycode/08/0855.py
@@ -0,0 +1,31 @@
+# Exam Room
+
+# There is an exam room with n seats in a single row labeled from 0 to n - 1.
+
+# When a student enters the room, they must sit in the seat that maximizes the distance to the closest person. If there are multiple such seats, they sit in the seat with the lowest number. If no one is in the room, then the student sits at seat number 0.
+
+# Design a class that simulates the mentioned exam room.
+
+# Implement the ExamRoom class:
+
+#     ExamRoom(int n) Initializes the object of the exam room with the number of the seats n.
+#     int seat() Returns the label of the seat at which the next student will set.
+#     void leave(int p) Indicates that the student sitting at seat p will leave the room. It is guaranteed that there will be a student sitting at seat p.
+
+# Constraints:
+
+#     1 <= n <= 109
+#     It is guaranteed that there is a student sitting at seat p.
+#     At most 104 calls will be made to seat and leave.
+
+class ExamRoom:
+
+    def __init__(self, n: int):
+        
+        
+    def seat(self) -> int:
+        
+        
+
+    def leave(self, p: int) -> None:
+        
