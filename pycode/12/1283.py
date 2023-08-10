f3474ddbca4d960aae962a8281ebe279eb0cb00f
izaxs <52440839+izaxs@users.noreply.github.com>
2023-08-09 23:53:38 -0700

    Update LC1283 pycode, use bisect, hacky

--- pycode/12/1283.py
+++ pycode/12/1283.py
@@ -1,9 +1,12 @@
-# Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
+# Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. 
+
+# Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
 
 # Each result of the division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
 
 # The test cases are generated so that there will be an answer.
 import math
+import bisect
 
 class Solution:
     def smallestDivisor(self, nums: list[int], threshold: int) -> int:
@@ -13,6 +16,9 @@ class Solution:
         if threshold == loBound: return maxNum
         if threshold >= hiBound: return 1
         lo, hi = 2, maxNum-1
+
+        # if curSum <= threshold: target lies between [lo, mid]
+        # if curSum > threshold: target lies between (mid, hi]
         while lo < hi:
             mid = (lo+hi)//2
             curSum = sum(math.ceil(x/mid) for x in nums)
@@ -22,10 +28,15 @@ class Solution:
                 lo = mid+1
         return lo
     
+    def smallestDivisor2(self, nums: list[int], threshold: int) -> int:
+        return bisect.bisect_left([*range(1, h := max(nums)+1)], -threshold,
+            0, h-1, key=lambda x: -sum(math.ceil(i/x) for i in nums))+1
+
 if __name__ == "__main__":
     s = Solution()
     nums = [1,2,5,9]
     threshold = 6
-    res = s.smallestDivisor(nums, threshold)
+    res = s.smallestDivisor2(nums, threshold)
+    print(res)
         
         
