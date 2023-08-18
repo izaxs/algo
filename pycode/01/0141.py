c34a68197243cf9f111b25230d98c56013fd9414
izaxs <52440839+izaxs@users.noreply.github.com>
2023-08-18 15:40:28 -0700

    Update LC0141.py

--- pycode/01/0141.py
+++ pycode/01/0141.py
@@ -1,6 +1,19 @@
 import include
 from linkedlist import Optional, ListNode
 
+# Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+# Return true if there is a cycle in the linked list. Otherwise, return false.
+
+# Constraints:
+
+#     The number of the nodes in the list is in the range [0, 104].
+#     -105 <= Node.val <= 105
+#     pos is -1 or a valid index in the linked-list.
+
+
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         slow = fast = head
@@ -11,3 +24,14 @@ class Solution:
             if slow == fast:
                 return True
         return False
+
+    def hasCycle2(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            assert slow
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+            
