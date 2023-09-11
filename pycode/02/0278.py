4eab8e2d9df43f1e225162d93eb9fb170bc7c0b6
izaxs <52440839+izaxs@users.noreply.github.com>
2023-09-11 01:33:17 -0500

    Update LC0278.py

--- pycode/02/0278.py
+++ pycode/02/0278.py
@@ -1,3 +1,15 @@
+# 278. First Bad Version
+
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+# You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+# Constraints:
+
+#     1 <= bad <= n <= 231 - 1
+
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 import random
@@ -18,3 +30,14 @@ class Solution:
             else:
                 lo = mid+1
         return lo
+    
+    def firstBadVersion2(self, n: int) -> int:
+        lo, hi = 1, n
+        while lo < hi:
+            mid = lo + ((hi - lo) >> 1)
+            if isBadVersion(mid):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
+
